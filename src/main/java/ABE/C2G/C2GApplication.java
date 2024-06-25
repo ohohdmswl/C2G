3c781136b9f360731647f32fe56ad14ae0e390b6
ohohdmswl <ohohdmswl@naver.com>
2024-06-25 11:08:07 +0900

    first commit, Spring Security 비활성화

---
+++ src/main/java/ABE/C2G/C2GApplication.java
@@ -0,0 +1,13 @@
+package ABE.C2G;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C2GApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C2GApplication.class, args);
+	}
+
+}
